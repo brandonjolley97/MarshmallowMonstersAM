0418f9e17dcd0753becadcceb516842c14a4dc01
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-09-15 09:56:31 -0600

    Added input stuff (Scanners)

--- src/marshmallows/controller/MonsterController.java
+++ src/marshmallows/controller/MonsterController.java
@@ -1,12 +1,14 @@
 package marshmallows.controller;
 
 import marshmallow.model.MarshmallowMonster;
+import java.util.Scanner;
 import marshmallows.view.MarshmallowOutput;
 
 public class MonsterController
 {
 	private MarshmallowMonster bolinMonster;
 	private MarshmallowOutput myOutput;
+	private Scanner monsterScanner;
 	
 	public MonsterController()
 	{
@@ -17,6 +19,7 @@ public class MonsterController
 		boolean hasBellyButton = true;
 		String name = "Tragdor the Burninator";
 		
+		monsterScanner = new Scanner(System.in);
 		myOutput = new MarshmallowOutput();
 		bolinMonster = new MarshmallowMonster(name, eyes, noses, hairs, legs, hasBellyButton);
 	}
@@ -30,6 +33,23 @@ public class MonsterController
 	
 	private void askQuestions()
 	{
+		System.out.println("Type a better name for the monster");
+		String betterMonsterName = monsterScanner.nextLine();
+		bolinMonster.setMonsterName(betterMonsterName);
+		System.out.println("Type in a better number of eyes for the monster");
+		int betterMonsterEyes = monsterScanner.nextInt();
+		bolinMonster.setMonsterEyes(betterMonsterEyes);
+		System.out.println("Type in a better number of noses for the monster");
+		int betterMonsterNoses = monsterScanner.nextInt();
+		bolinMonster.setMonsterNoses(betterMonsterNoses);
+		System.out.println("Type in a better number of hairs for the monster");
+		double betterMonsterHairs = monsterScanner.nextDouble();
+		bolinMonster.setMonsterHairs(betterMonsterHairs);
+		System.out.println("Type in a better number of legs for the monster");
+		double betterMonsterLegs = monsterScanner.nextDouble();
+		bolinMonster.setMonsterLegs(betterMonsterLegs);
+		System.out.println("")
+		
 		
 	}
 }
